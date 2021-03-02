ad78962e05c19123e2266c9750f99279672b0183
NGG-kang <skarndrkd1222@gmail.com>
2021-03-02 23:18:24 +0900

    Custom User

--- MixedDjango/clonecodding/accounts/models.py
+++ MixedDjango/clonecodding/accounts/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField(max_length=254, blank=True)
