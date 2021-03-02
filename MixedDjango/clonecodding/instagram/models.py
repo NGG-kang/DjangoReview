12f47585c887ce03b58b5885264c072ae7cb9399
NGG-kang <skarndrkd1222@gmail.com>
2021-03-02 23:15:06 +0900

    project instagram Model

--- MixedDjango/clonecodding/instagram/models.py
+++ MixedDjango/clonecodding/instagram/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    message = models.CharField(max_length=300)
+    photo = models.ImageField(upload_to="instagram/post/%Y/%m/%d")
+    tag = models.ManyToManyField('Tag', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    like_user = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="like_post_set")
+
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment = models.TextField()
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+
