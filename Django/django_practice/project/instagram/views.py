77879bde29b5fa30f9915b9bffdd116363e55c31
NGG-kang <skarndrkd1222@gmail.com>
2021-03-05 23:54:29 +0900

    Revert "Revert "Revert "url edu Update"""
    This reverts commit 872603f66f8debf3325846319b049e8b177a0511.

--- Django/django_practice/project/instagram/views.py
+++ Django/django_practice/project/instagram/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .models import Post
 # Create your views here.
 
@@ -15,9 +15,3 @@ def post_list(request):
         'q': q,
     })
 
-
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'instagram/post_detail.html', {
-        'post': post,
-    })
