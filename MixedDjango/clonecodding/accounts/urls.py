70cfca0b4601d774f72f295c2675d1228fecf9d6
NGG-kang <skarndrkd1222@gmail.com>
2021-03-09 21:29:06 +0900

    Signup, login, logout Create

--- MixedDjango/clonecodding/accounts/urls.py
+++ MixedDjango/clonecodding/accounts/urls.py
@@ -1,3 +1,11 @@
-from django.urls import path
+from django.contrib.auth.views import LogoutView
+from django.urls import path,reverse_lazy
+from . import views
 
-urlpatterns=[]
+app_name='accounts'
+
+urlpatterns=[
+    path('login/', views.login_view, name='login'),
+    path('singup/', views.signup_view, name='signup'),
+    path('logout/', views.logout_veiw, name='logout'),
+]
--- MixedDjango/clonecodding/accounts/views.py
+++ MixedDjango/clonecodding/accounts/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
-
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect
+from .forms import UserForm
+from django.urls import reverse_lazy
+from django.contrib.auth import get_user_model, login
 # Create your views here.
+
+
+class LoginView(LoginView):
+    model = get_user_model()
+    template_name = 'accounts/login.html'
+    next_page = reverse_lazy('instagram:post_list')
+
+
+def signup_view(request):
+
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('instagram:post_list')
+    else:
+        form = UserForm
+    return render(request, 'accounts/signup.html', {
+        'form': form,
+    })
+
+
+class LogoutView(LogoutView):
+    next_page = reverse_lazy('instagram:post_list')
+
+
+login_view = LoginView.as_view()
+logout_veiw = LogoutView.as_view()
