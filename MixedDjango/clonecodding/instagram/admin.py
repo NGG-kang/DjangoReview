ed647e30e4cdef6a79505e23148f1ae82e770e76
NGG-kang <skarndrkd1222@gmail.com>
2021-03-10 22:35:12 +0900

    add PostAdmin list_display

--- MixedDjango/clonecodding/instagram/admin.py
+++ MixedDjango/clonecodding/instagram/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
 from .models import Post, Comment, Tag
 # Register your models here.
-admin.site.register(Post)
+
 admin.site.register(Comment)
 admin.site.register(Tag)
+
+@admin.register(Post)
+class PostAdmin(ModelAdmin):
+    list_display = ['author', 'message']
