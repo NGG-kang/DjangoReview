ec5690e99a58de4b018cf77a3dc494d12dbd2bfd
NGG-kang <skarndrkd1222@gmail.com>
2021-03-11 22:23:17 +0900

    add message widgets, CommentForm

--- MixedDjango/clonecodding/instagram/forms.py
+++ MixedDjango/clonecodding/instagram/forms.py
@@ -1,12 +1,18 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['message', 'photo', 'tag']
+        widgets = {'message': forms.Textarea}
 
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['comment']
 
 
 
