ee398f200f810cbae004ed44bb2d8f1a4f1d2e29
NGG-kang <skarndrkd1222@gmail.com>
2021-03-13 23:51:49 +0900

    modify redirectview
    modify redirectview

--- MixedDjango/clonecodding/clonecodding/urls.py
+++ MixedDjango/clonecodding/clonecodding/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('instagram/', include('instagram.urls')),
     path('accounts/', include('accounts.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
-    path('', RedirectView.as_view(pattern_name='instagram:post_list'), name='root'),
+    path('', RedirectView.as_view(pattern_name='accounts:profile'), name='root'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
