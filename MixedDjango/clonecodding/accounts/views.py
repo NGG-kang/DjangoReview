44063b3e97f0be71d77438f4c15bc1abca48c74b
NGG-kang <skarndrkd1222@gmail.com>
2021-03-14 21:47:24 +0900

    add profile_eidt, follow, follow_recommend, follow_list

--- MixedDjango/clonecodding/accounts/views.py
+++ MixedDjango/clonecodding/accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
@@ -6,7 +7,7 @@ from django.views.generic import ListView
 from .forms import UserForm, ProfileForm
 from django.urls import reverse_lazy, reverse
 from django.contrib.auth import get_user_model, login
-from .models import Profile
+from .models import Profile, User
 from instagram.models import Post
 
 
@@ -17,7 +18,6 @@ from instagram.models import Post
 class LoginView(LoginView):
     model = get_user_model()
     template_name = 'accounts/login.html'
-    next_page = reverse_lazy('instagram:post_list')
 
 
 def signup_view(request):
@@ -52,19 +52,58 @@ def profile_view(request):
 @login_required
 def profile_edit(request):
     if request.POST:
-        profile_form = ProfileForm(request.POST)
+        profile_form = ProfileForm(request.POST, request.FILES)
         if profile_form.is_valid():
             profile = profile_form.save(commit=False)
             profile.user = request.user
             profile.save()
-        return redirect('profile/')
+        return redirect('accounts:profile')
     profile = Profile.objects.get(pk=request.user.pk)
     profile_form = ProfileForm(instance=profile)
     return render(request, 'accounts/profile_edit_form.html', {
         'form': profile_form,
     })
 
+@login_required
+def follow_recommend(request):
+    profile = get_object_or_404(Profile, user_id=request.user.pk)
+    suggested_user_list = get_user_model().objects.all()\
+            .exclude(pk=request.user.pk)\
+            .exclude(pk__in=request.user.following_set.all())
 
+    return render(request, 'accounts/profile.html', {
+        'profile': profile,
+        'suggested_user_list': suggested_user_list
+    })
+
+@login_required
+def follower_list(request):
+    profile = get_object_or_404(Profile, user_id=request.user.pk)
+    follower_list = get_user_model().objects.all()\
+            .exclude(pk=request.user.pk)\
+            .filter(pk__in=request.user.following_set.all())
+
+    return render(request, 'accounts/profile.html', {
+        'profile': profile,
+        'follower_list': follower_list
+    })
+
+def user_follow(request, username):
+    follow_user = get_object_or_404(User, username=username, is_active=True)
+    request.user.following_set.add(follow_user)
+    follow_user.following_set.add(request.user)
+    messages.success(request, f"{follow_user}님을 팔로우 했습니다.")
+    redirect_url = request.META.get("HTTP_REFERER", "root")
+    return redirect(redirect_url)
+
+@login_required
+def user_unfollow(request, username):
+    unfollow_user = get_object_or_404(User, username=username, is_active=True)
+    request.user.following_set.remove(unfollow_user)
+    unfollow_user.following_set.remove(request.user)
+    messages.success(request, f"{unfollow_user}님을 언팔로우 했습니다.")
+    redirect_url = request.META.get("HTTP_REFERER", "root")
+    return redirect(redirect_url)
 
 class ProfileView(ListView):
 
@@ -92,10 +131,6 @@ class ProfileView(ListView):
             'post_list': None
         })
 
-
-
-class ProfileView(LoginRequiredMixin, ListView):
-    template_name = 'accounts/profile.html'
 
 
 
