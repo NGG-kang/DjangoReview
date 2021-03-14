92d749145bd3bd69d0c60c600dd4848f5ed16ec4
NGG-kang <skarndrkd1222@gmail.com>
2021-03-14 21:48:43 +0900

    add post_like, unlike

--- MixedDjango/clonecodding/instagram/urls.py
+++ MixedDjango/clonecodding/instagram/urls.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path
-from .views import post_list, post_update, post_create, post_detail, post_delete
-
+from .views import post_list, post_update, post_create, post_detail, post_delete, post_like, post_unlike
 
 app_name = 'instagram'
 urlpatterns = [
@@ -10,6 +9,8 @@ urlpatterns = [
     path('<int:pk>/', post_detail, name='post_detail'),
     path('<int:pk>/post_update/', post_update, name='post_update'),
     path('<int:pk>/post_delete/', post_delete, name='post_delete'),
-    path('login/', LoginView.as_view()),
+    path('<int:pk>/post_delete/', post_delete, name='post_delete'),
+    path('<int:pk>/post_like/', post_like, name='post_like'),
+    path('<int:pk>/post_unlike/', post_unlike, name='post_unlike'),
 ]
 
