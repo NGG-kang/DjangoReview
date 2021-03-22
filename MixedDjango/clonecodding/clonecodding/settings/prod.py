e52d1c83efaa4b389fdcac71281d6e2f04668241
NGG-kang <skarndrkd1222@gmail.com>
2021-03-22 22:39:08 +0900

    divide settings.py (common, dev, prod) && add azure settings

---
+++ MixedDjango/clonecodding/clonecodding/settings/prod.py
@@ -0,0 +1,10 @@
+from .common import *
+
+STATICFILES_STORAGE = "clonecodding.storages.StaticAzureStorage"
+DEFAULT_FILE_STORAGE = "clonecodding.storages.MediaAzureStorage"
+
+# AZURE_ACCOUNT_NAME = os.environ["AZURE_ACCOUNT_NAME"]
+# AZURE_ACCOUNT_KEY = os.environ["AZURE_ACCOUNT_KEY"]
+
+AZURE_ACCOUNT_NAME = "skarndrkdtestdj"
+AZURE_ACCOUNT_KEY = "LzmbOUpT17PQRpOwgWnZP+vCpMZPhrzZHz/h0nTKhg7qKtMYOFK+n3boefM7Er8I3uPsFS31FcZbyn589RC25g=="
