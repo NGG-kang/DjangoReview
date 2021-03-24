a477f742af8d82168f3f98e658f462ba31d8fb01
NGG-kang <skarndrkd1222@gmail.com>
2021-03-24 23:04:20 +0900

    Update urls.py

--- MixedDjango/clonecodding/clonecodding/urls.py
+++ MixedDjango/clonecodding/clonecodding/urls.py
@@ -25,15 +25,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('instagram/', include('instagram.urls')),
     path('accounts/', include('accounts.urls')),
-
     path('', RedirectView.as_view(pattern_name='accounts:profile'), name='root'),
     path('api-auth/', include('rest_framework.urls')),
     path('api-jwt-auth/', obtain_jwt_token),
     path('api-jwt-auth/refresh/', refresh_jwt_token),
     path('api-jwt-auth/verify/', verify_jwt_token),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 if settings.DEBUG == True:
     import debug_toolbar
     urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
