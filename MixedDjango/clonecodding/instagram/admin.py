a319b7e1f0f8ef3bb127ba599f16537d9baf01c1
NGG-kang <skarndrkd1222@gmail.com>
2021-03-27 23:44:07 +0900

    add comment admin

--- MixedDjango/clonecodding/instagram/admin.py
+++ MixedDjango/clonecodding/instagram/admin.py
@@ -4,9 +4,15 @@ from django.contrib.admin import ModelAdmin
 from .models import Post, Comment, Tag
 # Register your models here.
 
-admin.site.register(Comment)
+
 admin.site.register(Tag)
+
 
 @admin.register(Post)
 class PostAdmin(ModelAdmin):
     list_display = ['author', 'message']
+
+
+@admin.register(Comment)
+class CommentAdmin(ModelAdmin):
+    list_display = ['author', 'post', 'comment']
